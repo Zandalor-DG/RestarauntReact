effb46ebca1e95bc6885a759d8224cc5823f46ea
Zandalor-DG <Paliy.zdg@gmail.com>
2020-07-20 18:11:23 +0300

    Create new version react

---
+++ RestarauntReact.UI/ClientApp/src/blogApp/containers/foodCategory/foodCategory.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+export default class FoodCategory extends React.Component {
+    render() {
+        return (
+                <div>Еда</div>
+            );
+    }
+};
